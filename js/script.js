cde8038c6ba54874184742ad01e1d0012f01c18d
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-22 15:14:50 +0100

    add milestone 1

--- js/script.js
+++ js/script.js
@@ -5,17 +5,19 @@ let result;
 let discount;
 
 sendBtn.addEventListener('click', function(){
-    // reset
-    discount = 1;
-
-    if(parseInt(age.value) < 18){
-        discount -= 0.175;
-    }
-    else if(parseInt(age.value) >= 65){
-        discount -= 0.333;
-    }
-    result = 0.2762 * parseFloat(distance.value) * discount;
+    if(parseInt(distance.value) > 0 && parseInt(age.value) > 0){
+        // reset
+        discount = 1;
     
-    // output finale
-    console.log(result.toFixed(2));
+        if(parseInt(age.value) < 18){
+            discount -= 0.175;
+        }
+        else if(parseInt(age.value) >= 65){
+            discount -= 0.333;
+        }
+        result = 0.2762 * parseFloat(distance.value) * discount;
+        
+        // output finale
+        console.log(result.toFixed(2));
+    }
 });
