06a050bb73e1470a57764528f998c169a0c3310f
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-22 19:33:10 +0100

    implement ticket price and js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,52 @@
-const distance = document.getElementById('distance');
+// form elements
+const nome = document.getElementById('name');
+const surname = document.getElementById('surname');
 const age = document.getElementById('age');
+const distance = document.getElementById('distance');
 const sendBtn = document.getElementById('sendBtn');
-let result;
-let discount;
+// price element
+const client = document.getElementById('client-data');
+const initialPrice = document.getElementById('initial-price');
+const minorDiscount = document.getElementById('minor-discount');
+const overDiscount = document.getElementById('over-discount');
+const finalPrice = document.getElementById('final-price');
+
+let finalP, initialP;
+let discount = {
+    minor : 0,
+    over : 0,
+    total : 1
+};
 
 sendBtn.addEventListener('click', function(){
     if(parseInt(distance.value) > 0 && parseInt(age.value) > 0){
         // reset
-        discount = 1;
+        discount = {
+            minor : 0,
+            over : 0,
+            total : 1
+        };
     
         if(parseInt(age.value) < 18){
-            discount -= 0.175;
+            discount.minor = 0.175
+            discount.total -= discount.minor;
         }
         else if(parseInt(age.value) >= 65){
-            discount -= 0.333;
+            discount.over = 0.333;
+            discount.total -= discount.over;
         }
-        result = 0.2762 * parseFloat(distance.value) * discount;
+        initialP = 0.2762 * parseFloat(distance.value);
+        finalP = initialP * discount.total;
         
         // output finale
+        client.innerHTML = `${nome.value} ${surname.value}, ${age.value}y/o`;
+        initialPrice.innerHTML = `\$${initialP.toFixed(2)}`;
+        minorDiscount.innerHTML = `- \$${discount.minor.toFixed(2)}`;
+        overDiscount.innerHTML = `- \$${discount.over.toFixed(2)}`;
+        finalPrice.innerHTML = `\$${finalP.toFixed(2)}`;
         console.log(result.toFixed(2));
+
+        document.h
+         = '#price';
     }
 });
